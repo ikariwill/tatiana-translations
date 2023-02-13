501c50062fb19247e47a93d4c7b851074cd98fc7
Willian Silva <willian@wiki4fit.com.br>
2023-02-13 14:40:59 -0300

    feat: impressum page

---
+++ src/pages/impressum.tsx
@@ -0,0 +1,205 @@
+import FeaturedMedia from "@components/FeaturedMedia";
+import Header from "@components/Header";
+import Head from "next/head";
+
+export default function Impressum() {
+  return (
+    <>
+      <Head>
+        <title>Tatiana Hernandez | Translations & Revisions</title>
+      </Head>
+      <Header />
+      <div className="content clearfix">
+        <FeaturedMedia title="Impressum" />
+      </div>
+
+      <section className="section-block bkg-grey-ultralight">
+        <div className="row">
+          <div className="column width-8 justify">
+            <h3>Site notice</h3>
+            <p>
+              Publication information pursuant to Section 5 German Telemedia
+              Services Act (TMG) and Section 55 German Interstate Treaty on
+              Broadcasting (RStV)
+              <br />
+              <br />
+              Tatiana Hernandez Zaidan
+              <br />
+              Poppenreuther Strasse 67
+              <br />
+              90419 Nürnberg
+              <br />
+              VAT ID: DE314875688
+            </p>
+            <strong>Contact</strong>
+            <br />
+            Phone: +49 163 7827586
+            <br />
+            E-Mail: tatiher.translator@hotmail.com
+            <br />
+            <br />
+            <br />
+            <strong>Photography credits</strong>
+            <ul>
+              <li>- Vintage Globe - Artem Beliaikin</li>
+              <li>- Woman holding an iPhone - Firmbee.com</li>
+              <li>- Brainstorming over paper - Scott Graham</li>
+              <li>- Medicine industry - Louis Reed</li>
+              <li>- Cyber Security - FLY:D</li>
+              <li>- Country flags icons - Good Ware</li>
+            </ul>
+            <br />
+            <p>
+              EU Online Dispute Resolution platform
+              <br />
+              Please note that the web-based ODR platform designed to help
+              consumers who have bought goods or services online and
+              subsequently have a problem with their online purchases has been
+              operational since 9 January 2016 and can be accessed at:
+              https://ec.europa.eu/consumers/odr/.
+            </p>
+            <strong>Liability for website content</strong>
+            <br />
+            <p>
+              This website has been compiled with the greatest possible care and
+              attention, nevertheless, no guarantee for the correctness and
+              accuracy of the information it contains can be given. No liability
+              will be accepted for any material or non-material third party loss
+              or damage caused by the use of this website or of information made
+              available for download. I reserve myself the right to modify,
+              supplement, or delete any or all of the information offered on the
+              website.
+            </p>
+            <strong>Liability for the content of linked websites</strong>
+            <br />
+            <p>
+              This website includes links to external sites and/or servers that
+              are not within my control or responsibility. I do not assume any
+              liability for external links to other content, notwithstanding the
+              careful checks which are conducted on such content before links
+              are made. Responsibility for the content of linked pages lies
+              wholly with their respective operators. Shall I determine that a
+              linked external site contains content which may give rise to civil
+              or criminal liability, or be notified of the existence of such
+              content by a third party, I will immediately remove any link to
+              this site. I expressly dissociate myself from any such content.
+            </p>
+            <br />
+            <strong>Copyright</strong>
+            <br />
+            <p>
+              All content and images published on this web site are subject – in
+              the absence of acknowledgement of another source – to the
+              copyright of Tatiana Hernandez Zaidan. Where content and images
+              published on this website have not been created by the operator of
+              the website, the copyright of third parties has been observed.
+              Content created by third parties is acknowledged as such. If you
+              should nonetheless notice a violation of copyright, please inform
+              us. As soon as we are made aware of any violations of copyright,
+              we will remove the offending content or images without delay.
+            </p>
+            <br />
+            <hr />
+            <br />
+            <h3>Impressum</h3>
+            <p>
+              Angaben gemäß § 5 TMG
+              <br />
+              Tatiana Hernandez Zaidan
+              <br />
+              Poppenreuther Strasse 67
+              <br />
+              90419 Nürnberg
+            </p>
+            <strong>Kontakt</strong>
+            <br />
+            Telefon: +49 163 7827586
+            <br />
+            E-Mail: tatiher.translator@hotmail.com
+            <br />
+            Umsatzsteuer-ID
+            <br />
+            Umsatzsteuer-Identifikationsnummer gemäß § 27 a Umsatzsteuergesetz:
+            DE314875688
+            <br />
+            <br />
+            <strong>Bildnachweise</strong>
+            <ul>
+              <li>- Vintage Globe - Artem Beliaikin</li>
+              <li>- Woman holding an iPhone - Firmbee.com</li>
+              <li>- Brainstorming over paper - Scott Graham</li>
+              <li>- Medicine industry - Louis Reed</li>
+              <li>- Cyber Security - FLY:D</li>
+              <li>- Country flags icons - Good Ware</li>
+            </ul>
+            EU-Streitschlichtung
+            <br />
+            Die Europäische Kommission stellt eine Plattform zur
+            Online-Streitbeilegung (OS) bereit:
+            https://ec.europa.eu/consumers/odr/. Unsere E-Mail-Adresse finden
+            Sie oben im Impressum.
+            <br />
+            <br />
+            Verbraucherstreitbeilegung/Universalschlichtungsstelle
+            <br />
+            Wir sind nicht bereit oder verpflichtet, an
+            Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle
+            teilzunehmen.
+            <br />
+            <br />
+            <strong>Haftung für Inhalte</strong>
+            <br />
+            <p>
+              Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene
+              Inhalte auf diesen Seiten nach den allgemeinen Gesetzen
+              verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter
+              jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde
+              Informationen zu überwachen oder nach Umständen zu forschen, die
+              auf eine rechtswidrige Tätigkeit hinweisen.
+            </p>
+            <p>
+              Verpflichtungen zur Entfernung oder Sperrung der Nutzung von
+              Informationen nach den allgemeinen Gesetzen bleiben hiervon
+              unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem
+              Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich.
+              Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir
+              diese Inhalte umgehend entfernen.
+            </p>
+            <br />
+            <strong>Haftung für Links</strong>
+            <p>
+              Unser Angebot enthält Links zu externen Websites Dritter, auf
+              deren Inhalte wir keinen Einfluss haben. Deshalb können wir für
+              diese fremden Inhalte auch keine Gewähr übernehmen. Für die
+              Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter
+              oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten
+              wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße
+              überprüft. Rechtswidrige Inhalte waren zum Zeitpunkt der
+              Verlinkung nicht erkennbar. Eine permanente inhaltliche Kontrolle
+              der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer
+              Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von
+              Rechtsverletzungen werden wir derartige Links umgehend entfernen.
+            </p>
+            <br />
+            <strong>Urheberrecht</strong>
+            <p>
+              Die durch die Seitenbetreiber erstellten Inhalte und Werke auf
+              diesen Seiten unterliegen dem deutschen Urheberrecht. Die
+              Vervielfältigung, Bearbeitung, Verbreitung und jede Art der
+              Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der
+              schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers.
+              Downloads und Kopien dieser Seite sind nur für den privaten, nicht
+              kommerziellen Gebrauch gestattet. Soweit die Inhalte auf dieser
+              Seite nicht vom Betreiber erstellt wurden, werden die
+              Urheberrechte Dritter beachtet. Insbesondere werden Inhalte
+              Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine
+              Urheberrechtsverletzung aufmerksam werden, bitten wir um einen
+              entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen
+              werden wir derartige Inhalte umgehend entfernen.
+            </p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
